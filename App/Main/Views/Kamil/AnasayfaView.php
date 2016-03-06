809238ffe3c72f760089361541cc0a1045be1c0c
kamilkaplan <kamilkaplnn@hotmail.com>
2016-03-06 17:50:37 +0200

    for desing

---
+++ App/Main/Views/Kamil/AnasayfaView.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Abdurrahman Abii</title>
+<link rel="stylesheet" href="<?php echo get::site(); ?>/css/style2.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo get::site(); ?>/css/menu.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo get::site(); ?>/css/slider.css" type="text/css" />
+<script type="text/javascript" src="<?php echo get::site(); ?>/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="<?php echo get::site(); ?>/js/slider.js"></script>
+<script type="text/javascript" src="<?php echo get::site(); ?>/js/site.js"></script>
+</head>
+<body>
+<div class="kapsa">
+    	<div class="header">
+            
+            <div class="ihtiyac">İhtiyaç Sahibiyim</div>
+          <div class="gonullu">Gönüllüyüm</div>
+  </div>
+        <ul id="menu" class="black" style=" margin-top:20px">
+    		<li class="active"><a href="index.html">Anasayfa</a></li>    		
+			<li><a href="paylas-takipet.html">Paylas / Takip Et</a></li>
+            <li><a href="hakkımızda.html">Hakkımızda</a></li>              
+ 		</ul>
+   <div id="slider">
+     <div class="slider">
+        <ul>
+         <li><a href=""><img src="<?php echo get::site(); ?>/images/slider/img/img1.png" alt="" /></a></li>
+         <li><a href=""><img src="<?php echo get::site(); ?>/images/slider/img/img2.png" alt="" /></a></li>
+         <li><a href=""><img src="<?php echo get::site(); ?>/images/slider/img/img3.png" alt="" /></a></li>
+         <li><a href=""><img src="<?php echo get::site(); ?>/images/slider/img/img4.png" alt="" /></a></li>
+        </ul>
+     </div>
+      <div class="sliderbutton">
+         <a href="#" class="onceki"><img src="<?php echo get::site(); ?>/images/slider/prev2.png" /></a>
+         <a href="#" class="sonraki"><img src="<?php echo get::site(); ?>/images/slider/next2.png" /></a>
+      </div>
+   </div>
+   <div class="sozkosesi">
+   AS
+   </div>
+   <div class="footer">© Web Desing</div>
+</div>
+</body>
+</html>
